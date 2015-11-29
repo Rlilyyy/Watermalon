e0e34d524fee7d5f9f9ecf2fd0d38f31b862acc7
BingoLee <bingohlee@gmail.com>
2015-11-29 16:54:19 +0800

    添加一些后台处理样式

--- index/getNews.php
+++ index/getNews.php
@@ -12,9 +12,13 @@
 	// 为了让手机显示正常加的meta
 	$meta = '<meta charset="utf-8"><meta name="format-detection" content="telephone=no,email=no"><meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no"><meta name="full-screen" conten="yes">';
 	// 将headline的div隐藏
-	$headline = '.headline {' . 'display:none;}';
+	$headline = '.headline {' . 'display:none;}body {margin:0px;}.main-wrap {margin:8px;}';
+	// 增加LOGO
+	$header = "<header>Watermalon</header>";
+	// 增加一些神奇的CSS
+	$headerCSS = "header {width: 100%;height: 45px;line-height: 45px;text-align: center;background-color: rgb(150,205,205);font-size: 1.25rem;color: white;font-family:\"Microsoft YaHei\", \"微软雅黑\", Georgia, \"Times New Roman\", STXihei, \"华文细黑\", serif}";
 	// 将要显示的html文本
-	$html = $meta . '<style>' . $headline . $css . '</style>' . '<body>' . $arr['body'] . '</body>';
+	$html = $meta . '<style>' . $headerCSS . $headline . $css . '</style>' . '<body>' . $header . $arr['body'] . '</body>';
 	// 将所有图片的链接地址转换为经过后台处理的地址
 	$reg = '/[\'"](http.*?)(jpg|png)[\'"]/';
 	echo preg_replace($reg,"http://localhost/Watermalon/index/getImage.php?id=$1$2",$html);
